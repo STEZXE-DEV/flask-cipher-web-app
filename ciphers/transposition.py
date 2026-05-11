a221c1bc0ca88b9101bb2e0a5b1f670d54e8e6cf
STEZXE-DEV <stanislawjurek.ina@gmail.com>
2026-05-12 00:07:40 +0200

    created for transposition: encrypt and decrypt functions

--- ciphers/transposition.py
+++ ciphers/transposition.py
@@ -1,7 +1,38 @@
+import textwrap as tx
+
 # Kod odpowiadający za logikę szyfrowania i deszyfrowania metodą przestawieniową - skokową
 
 def encrypt(message, key):
-    pass
+
+    message = message.lower()
+    message = message.replace(" ", "")
+    message_length = len(message)
+    message_columns = ['' for x in range(key)]   
+
+    for i in range(message_length):
+        message_columns[i % key] += message[i]
+    encrypted_message = ''.join(message_columns)
+
+    return encrypted_message
+
     
 def decrypt(message, key):
-    pass
+    
+    decrypted_message = ''
+    message = message.lower()
+    message = message.replace(" ", "")
+    message_length = len(message)
+    message_columns = [ x for x in tx.wrap(message, key)]
+    print(message_columns)
+
+    for i in range(len(message_columns[0])):
+        for column in message_columns:
+            decrypted_message += column[i]
+
+    decrypted_message = ''.join(decrypted_message)
+    return decrypted_message
+
+
+# text1 = "żółta źółć"
+# print(encrypt(text1, 3))
+print(decrypt("żtóóałłźć", 3))
