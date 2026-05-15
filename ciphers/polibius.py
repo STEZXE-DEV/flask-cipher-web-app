093710faa4d5377eeb02581c40575159048a6831
STEZXE-DEV <stanislawjurek.ina@gmail.com>
2026-05-15 14:23:38 +0200

    Created encrypt and decrypt for polibius cipher.

--- ciphers/polibius.py
+++ ciphers/polibius.py
@@ -1,7 +1,55 @@
+from utility.util import normalize
+
 # Kod odpowiadający za logikę szyfrowania i deszyfrowania metodą Polibiusza
 
+# Alfabet bazowy, który pozwala na stworzenie siatki 6x6
+alphabet = "abcdefghijklmnopqrstuvwxyz0123456789"
+
 def encrypt(message, key):
-    pass
+
+
+    shifted_alphabet = alphabet[key:] + alphabet[:key]
+
+    # zamiana liter wiadomości na małe litery, usunięcie spacji i normalizacja polskich znaków diakrytycznych
+    message =  normalize(message.lower().replace(" ", ""))
+
+    position_dict = {}
+    
+    for i, letter in enumerate(shifted_alphabet):
+        position_dict[letter] = i
+        
+    encrypted_message = ''
+
+    for letter in message:
+        letter_position = position_dict[letter]
+        column = letter_position % 6 + 1
+        row = letter_position // 6 + 1
+        encrypted_message += str(row) + str(column) + ' '
+    
+    return encrypted_message
 
 def decrypt(message, key):
-    pass
+
+    key %= 36
+    shifted_alphabet = alphabet[key:] + alphabet[:key]
+
+    message = message.replace(" ", "")
+
+    decrypted_message = ''
+
+    for digit in range(0, len(message), 2):
+
+        '''WAŻNE: wymaga walidacji w backendzie, bo jeśli będzie nieparzysta długość wiadomości to wystąpi IndexError w message[digit+1]'''
+        number = [ message[digit], message[digit+1] ]   
+        row = int(number[0]) - 1
+        column = int(number[1]) - 1
+        letter_position = row * 6 + column
+        decrypted_message += shifted_alphabet[letter_position]
+
+    return decrypted_message
+
+# TO BE MOVED
+# alphabet = "abcdefghijklmnopqrstuvwxyz0123456789"
+# print(decrypt("12 11 31", 72))
+# print(encrypt("abgęą tralala 123", 1))
+# print(decrypt("66 11 16 14 66 41 35 66 25 66 25 66 53 54 55", 1))
